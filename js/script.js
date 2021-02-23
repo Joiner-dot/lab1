3bd25421ab4d9d04b4734f5dc01bcff5add52b94
George <egor_kuzmin22@mail.ru>
2021-02-23 12:56:58 +0300

    Commit2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function buttonwith() {
+
+    var width = screen.width
+    console.log(width)
+
+    if (width < 800)
+    {
+        document.getElementById("locationburron").innerHTML = "<img id=\"reloadicon\" src=\"image/93638.png\">"
+    }
+}
