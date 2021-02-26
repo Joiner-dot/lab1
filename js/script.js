e7944d7fe76631de35f8abd812d869a8d674ba2a
George <egor_kuzmin22@mail.ru>
2021-02-26 19:56:12 +0300

    Commit10

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function buttonwith() {
 
-    var width = screen.width
+    var width = document.documentElement.clientWidth
 
     if (width < 800) {
         document.getElementById("locationburron").innerHTML = "<img id=\"reloadicon\" src=\"image/93638.png\">"
