99724b7f950b7f62ed2531de973df07b711795d3
George <egor_kuzmin22@mail.ru>
2021-03-09 16:01:56 +0300

    Commit22

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ function buttonwith() {
     }
 }
 
-function printthisweater(res){
+function printthisweater(res) {
     document.querySelector('#thisweater').innerHTML =
         "<h2 id=\"currentlocation\">" + res.data.name + "</h2>\n" +
         "                <img id=\"currentlocationicon\" src=\"image/day_" + res.data.weather[0].main.toLowerCase() + ".png\">\n" +
-        "                <p id=\"currentgrad\">" + Math.ceil((res.data.main.temp)*10)/10 + "°C</p>\n" +
+        "                <p id=\"currentgrad\">" + Math.ceil((res.data.main.temp) * 10) / 10 + "°C</p>\n" +
         "                <ul id=\"contain\">\n" +
         "                    <li class=\"wind\">\n" +
         "                        <p class=\"hh\">Ветер</p>\n" +
@@ -43,10 +43,14 @@ function printthisweater(res){
 function showlocation(position) {
     var lat = position.coords.latitude
     var lon = position.coords.longitude
+
     let url = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lang=ru&units=metric&lat=${lat}&lon=${lon}`;
-    axios.get(url).then(res => {
+   try{ axios.get(url).then(res => {
         printthisweater(res)
-    })
+    })}
+    catch (e) {
+       alert(e)
+    }
 }
 
 function printothercity(res, count3) {
@@ -54,7 +58,7 @@ function printothercity(res, count3) {
     console.log(name)
     document.querySelector(name).innerHTML =
         "            <h3 class=\"nameofothercity\">" + res.data.name + "</h3>\n" +
-        "            <p class=\"graofothercity\">" + Math.ceil((res.data.main.temp)*10)/10 + "°C</p>\n" +
+        "            <p class=\"graofothercity\">" + Math.ceil((res.data.main.temp) * 10) / 10 + "°C</p>\n" +
         "            <img class=\"othercityicon\" src=\"image/day_" + res.data.weather[0].main.toLowerCase() + ".png\">\n" +
         "            <button class=\"x\">×</button>\n" +
         "            <ul class=\"details\">\n" +
@@ -81,6 +85,20 @@ function printothercity(res, count3) {
         "            </ul>\n"
 }
 
+function notReceived() {
+    alert("Ошибка вашего местоположения, поэтому берем дефолтный город")
+
+    city = "Москва"
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
+    try {
+        axios.get(url).then(res => {
+            printthisweater(res)
+        })
+    } catch (e) {
+        alert(e)
+    }
+}
+
 window.onload = function () {
     //кнопка
     var width = document.documentElement.clientWidth
@@ -91,13 +109,15 @@ window.onload = function () {
     }
     //текущие координаты
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showlocation)
+        console.log("123")
+        navigator.geolocation.getCurrentPosition(showlocation, notReceived, {timeout: 10000})
     } else {
         city = "Москва"
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
         axios.get(url).then(res => {
             printthisweater(res)
-        })    }
+        })
+    }
     //отсальные города
     for (let i = 0; i < window.localStorage.length; i++) {
         let city = window.localStorage.getItem(i)
@@ -111,9 +131,13 @@ window.onload = function () {
 function updatethisweather() {
     let city = document.getElementById("currentlocation").textContent
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-    axios.get(url).then(res => {
-        printthisweater(res)
-    })
+    try {
+        axios.get(url).then(res => {
+            printthisweater(res)
+        })
+    } catch (e) {
+        alert(e)
+    }
 }
 
 //новый город
@@ -124,12 +148,16 @@ function addcity() {
     console.log(window.localStorage)
     let city = document.getElementById("add").value
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-    axios.get(url).then(res => {
-        count2 = window.localStorage.length - 1
-        console.log(count2)
-        document.querySelector("#othercities").innerHTML += "<section id=\"city" + count2 + "\">\n" +
-            "            <h3>Загрузка</h3>\n" +
-            "        </section>"
-        printothercity(res, count2)
-    })
+    try {
+        axios.get(url).then(res => {
+            count2 = window.localStorage.length - 1
+            console.log(count2)
+            document.querySelector("#othercities").innerHTML += "<section id=\"city" + count2 + "\">\n" +
+                "            <h3>Загрузка</h3>\n" +
+                "        </section>"
+            printothercity(res, count2)
+        })
+    } catch (e) {
+        alert(e)
+    }
 }
