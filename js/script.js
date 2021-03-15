4ecdac814cb7d246190ea5669667ed60b6442860
George <egor_kuzmin22@mail.ru>
2021-03-15 16:21:37 +0300

    Commit 24

--- js/script.js
+++ js/script.js
@@ -1,165 +1,176 @@
-let apiKey = "275483d966e6fd1ad712f36660db4ea6";
-let count = 0
+let apiKey = window.localStorage.getItem("apiKey");
 
-function buttonwith() {
-
-    var width = document.documentElement.clientWidth
-    console.log(width)
-    if (width < 1000) {
-        document.getElementById("locationburron").innerHTML = "<img id=\"reloadicon\" src=\"image/93638.png\">"
-    } else {
-        document.getElementById("locationburron").innerHTML = "Обновить геолокацию"
+window.onload = function () {
+    //кнопка "обновить геолокацию"
+    buttonwith()
+    //текущие координаты
+        navigator.geolocation.getCurrentPosition(showlocation, notReceived, {timeout: 10000})
+    //остальные города
+    for (let i = 1; i < window.localStorage.length; i++) {
+        let city = window.localStorage.getItem(i)
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
+        getresotherweather(url, i)
     }
-}
-
-function printthisweater(res) {
-    document.querySelector('#thisweater').innerHTML =
-        "<h2 id=\"currentlocation\">" + res.data.name + "</h2>\n" +
-        "                <img id=\"currentlocationicon\" src=\"image/day_" + res.data.weather[0].main.toLowerCase() + ".png\">\n" +
-        "                <p id=\"currentgrad\">" + Math.ceil((res.data.main.temp) * 10) / 10 + "°C</p>\n" +
-        "                <ul id=\"contain\">\n" +
-        "                    <li class=\"wind\">\n" +
-        "                        <p class=\"hh\">Ветер</p>\n" +
-        "                        <p>" + res.data.wind.speed + " км/ч</p>\n" +
-        "                    </li>\n" +
-        "                    <li class=\"wind\">\n" +
-        "                        <p class=\"hh\">Облачность</p>\n" +
-        "                        <p>" + res.data.weather[0].description + "</p>\n" +
-        "                    </li>\n" +
-        "                    <li class=\"wind\">\n" +
-        "                        <p class=\"hh\">Давление</p>\n" +
-        "                        <p>" + res.data.main.pressure + " hpa</p>\n" +
-        "                    </li>\n" +
-        "                    <li class=\"wind\">\n" +
-        "                        <p class=\"hh\">Влажность</p>\n" +
-        "                        <p>" + res.data.main.humidity + "%</p>\n" +
-        "                    </li>\n" +
-        "                    <li class=\"wind\">\n" +
-        "                        <p class=\"hh\">Координаты</p>\n" +
-        "                        <p>[" + res.data.coord.lon + ", " + res.data.coord.lat + "]</p>\n" +
-        "                    </li>\n" +
-        "                </ul>"
 }
 
 function showlocation(position) {
     var lat = position.coords.latitude
     var lon = position.coords.longitude
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lang=ru&units=metric&lat=${lat}&lon=${lon}`;
-    try {
-        axios.get(url).then(res => {
-            printthisweater(res)
-        })
-    } catch (e) {
-        alert(e)
-    }
-}
-
-function printothercity(res, count3) {
-    let name = "#city" + count3
-    console.log(name)
-    document.querySelector(name).innerHTML =
-        "            <h3 class=\"nameofothercity\">" + res.data.name + "</h3>\n" +
-        "            <p class=\"graofothercity\">" + Math.ceil((res.data.main.temp) * 10) / 10 + "°C</p>\n" +
-        "            <img class=\"othercityicon\" src=\"image/day_" + res.data.weather[0].main.toLowerCase() + ".png\">\n" +
-        "            <button class=\"x\">×</button>\n" +
-        "            <ul class=\"details\">\n" +
-        "                <li class=\"wind\">\n" +
-        "                    <p class=\"hh\">Ветер</p>\n" +
-        "                    <p>" + res.data.wind.speed + " км/ч</p>\n" +
-        "                </li>\n" +
-        "                <li class=\"wind\">\n" +
-        "                    <p class=\"hh\">Облачность</p>\n" +
-        "                    <p>" + res.data.weather[0].description + "</p>\n" +
-        "                </li>\n" +
-        "                <li class=\"wind\">\n" +
-        "                    <p class=\"hh\">Давление</p>\n" +
-        "                    <p>" + res.data.main.pressure + " hpa</p>\n" +
-        "                </li>\n" +
-        "                <li class=\"wind\">\n" +
-        "                    <p class=\"hh\">Влажность</p>\n" +
-        "                    <p>" + res.data.main.humidity + "%</p>\n" +
-        "                </li>\n" +
-        "                <li class=\"wind\">\n" +
-        "                    <p class=\"hh\">Координаты</p>\n" +
-        "                    <p>[" + res.data.coord.lon + ", " + res.data.coord.lat + "]</p>\n" +
-        "                </li>\n" +
-        "            </ul>\n"
+    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lang=ru&units=metric&lat=${lat}&lon=${lon}`
+    getresthisweather(url)
 }
 
 function notReceived() {
     alert("Ошибка вашего местоположения, поэтому берем дефолтный город")
 
-    city = "Москва"
+    let city = "Москва"
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
+    getresthisweather(url)
+}
+
+//отправка запроса и печать погоды по геолокации
+async function getresthisweather(url) {
+    let res;
     try {
-        axios.get(url).then(res => {
-            printthisweater(res)
-        })
+        res = await axios.get(url)
+        printthisweater(res)
+    } catch (e) {
+        alert(e)
+    }
+
+}
+
+//отправка запроса и печать погоды избранных городов
+async function getresotherweather(url, i) {
+    let res;
+    try {
+        res = await axios.get(url)
+        printothercity(res, i)
     } catch (e) {
         alert(e)
     }
 }
 
-window.onload = function () {
-    //кнопка
-    var width = document.documentElement.clientWidth
+//Печать города по геолокации
+function printthisweater(res) {
+
+//Определяем значения
+    document.getElementById("currentgrad").textContent = Math.ceil((res.data.main.temp) * 10) / 10 + "°C"
+    document.getElementById("currentlocation").textContent = res.data.name
+    document.getElementById("currentlocationicon").src = "image/day_"
+        + res.data.weather[0].main.toLowerCase()
+        + ".png"
+
+    let ul = document.getElementById("contain")
+    ul.innerHTML = ""
+    addtolist(ul, res)
+
+}
+
+//Печать избранного города
+function printothercity(res, count3) {
+    let name = "#city" + count3
+
+    //Определяем значения
+    document.querySelector(name + " .graofothercity").textContent
+        = Math.ceil((res.data.main.temp) * 10) / 10 + "°C"
+    document.querySelector(name + " .nameofothercity").textContent = res.data.name
+    document.querySelector(name + " .othercityicon").src = "image/day_"
+        + res.data.weather[0].main.toLowerCase()
+        + ".png"
+
+
+    let ul = document.querySelector(name + " .details")
+    addtolist(ul, res)
+}
+
+//Обновить геолокацию
+function updatethisweather() {
+    navigator.geolocation.getCurrentPosition(showlocation, notReceived, {timeout: 10000})
+}
+
+//Работа с показателями ветра, давления, влажности и координат и добавление их в список
+function addtolist(ul, res) {
+
+    //Ветер
+    let li = document.createElement("li")
+    li.setAttribute("class", "wind")
+    li.innerHTML = "<p class=\"hh\">Ветер</p>\n" +
+        "               <p>" + res.data.wind.speed + " м/с</p>\n"
+    ul.appendChild(li)
+
+    //Облачность
+    li = document.createElement("li")
+    li.setAttribute("class", "wind")
+    li.innerHTML = "<p class=\"hh\">Облачность</p>\n" +
+        "           <p>" + res.data.weather[0].description + "</p>\n"
+    ul.appendChild(li)
+
+    //Дввление
+    li = document.createElement("li")
+    li.setAttribute("class", "wind")
+    li.innerHTML = "<p class=\"hh\">Давление</p>\n" +
+        "           <p>" + res.data.main.pressure + " hpa</p>\n"
+    ul.appendChild(li)
+
+    //Влажность
+    li = document.createElement("li")
+    li.setAttribute("class", "wind")
+    li.innerHTML = "<p class=\"hh\">Влажность</p>\n" +
+        "           <p>" + res.data.main.humidity + "%</p>\n"
+    ul.appendChild(li)
+
+    //Координаты
+    li = document.createElement("li")
+    li.setAttribute("class", "wind")
+    li.innerHTML = "<p class=\"hh\">Координаты</p>\n" +
+        "           <p>[" + res.data.coord.lon + ", " + res.data.coord.lat + "]</p>\n"
+    ul.appendChild(li)
+}
+
+function buttonwith() {
+
+    let width = document.documentElement.clientWidth;
     if (width < 1000) {
-        document.getElementById("locationburron").innerHTML = "<img id=\"reloadicon\" src=\"image/93638.png\">"
+        document.getElementById("locationburron").innerHTML = "<img id=\"reloadicon\" " +
+            "src=\"image/93638.png\">"
     } else {
         document.getElementById("locationburron").innerHTML = "Обновить геолокацию"
     }
-    //текущие координаты
-    if (navigator.geolocation) {
-        console.log("123")
-        navigator.geolocation.getCurrentPosition(showlocation, notReceived, {timeout: 10000})
-    } else {
-        city = "Москва"
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-        axios.get(url).then(res => {
-            printthisweater(res)
-        })
-    }
-    //отсальные города
-    for (let i = 0; i < window.localStorage.length; i++) {
-        let city = window.localStorage.getItem(i)
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-        axios.get(url).then(res => {
-            printothercity(res, i)
-        })
-    }
 }
 
-function updatethisweather() {
-    let city = document.getElementById("currentlocation").textContent
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-    try {
-        axios.get(url).then(res => {
-            printthisweater(res)
-        })
-    } catch (e) {
-        alert(e)
-    }
-}
+//Добавление нового города
+async function addcity() {
+    let res;
+    if (document.getElementById("add").value !== "") {
+        try {
+            let city = document.getElementById("add").value
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
+            res = await axios.get(url)
 
-//новый город
-function addcity() {
-    if (document.getElementById("add").value !== "")
-        window.localStorage.setItem(window.localStorage.length, document.getElementById("add").value)
-    count++
-    console.log(window.localStorage)
-    let city = document.getElementById("add").value
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-    try {
-        axios.get(url).then(res => {
-            count2 = window.localStorage.length - 1
-            console.log(count2)
-            document.querySelector("#othercities").innerHTML += "<section id=\"city" + count2 + "\">\n" +
-                "            <h3>Загрузка</h3>\n" +
-                "        </section>"
-            printothercity(res, count2)
-        })
-    } catch (e) {
-        alert(e)
+            //Проверка города на уже существующий город в списке
+            for (let i = 1; i < window.localStorage.length; i++) {
+                if (res.data.name === document.querySelector("#city" + i + " .nameofothercity").textContent) {
+                    throw("Город уже есть")
+                }
+            }
+            window.localStorage.setItem(window.localStorage.length, res.data.name)
+            let count = window.localStorage.length - 1
+            document.querySelector("#othercities").innerHTML +=
+                "<section id=\"city"+ count +"\">            " +
+                "<h3 class=\"nameofothercity\">Загрузка</h3>\n" +
+                "            <p class=\"graofothercity\"></p>\n" +
+                "            <img class=\"othercityicon\">\n" +
+                "            <button class=\"x\">×</button>\n" +
+                "            <ul class=\"details\">\n" +
+                "            </ul>\n" +
+                "</section>"
+
+            getresotherweather(url, window.localStorage.length - 1)
+
+        } catch (e) {
+            alert(e)
+        }
     }
 }
